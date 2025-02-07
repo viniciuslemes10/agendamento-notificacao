3a67b4f4b9d58ec1abee622d84b575264a65a849
viniciuslemes10 <vinikjhgfds@gmail.com>
2025-02-07 12:05:56 -0300

    feat: adicionando metodos para cancelamento de notificações

--- src/main/java/com/vinidev/agendamento_notificacao_api/business/mapper/IAgendamentoMapper.java
+++ src/main/java/com/vinidev/agendamento_notificacao_api/business/mapper/IAgendamentoMapper.java
@@ -4,6 +4,7 @@ import com.vinidev.agendamento_notificacao_api.controller.dto.in.AgendamentoRequ
 import com.vinidev.agendamento_notificacao_api.controller.dto.out.AgendamentoResponseDto;
 import com.vinidev.agendamento_notificacao_api.infrastruture.entities.Agendamento;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import static org.mapstruct.MappingConstants.ComponentModel.SPRING;
 
@@ -12,4 +13,9 @@ public interface IAgendamentoMapper {
 
     Agendamento paraEntity(AgendamentoRequestDto agendamento);
     AgendamentoResponseDto paraDto(Agendamento agendamento);
+
+
+    @Mapping(target = "dataHoraModificacao", expression = "java(LocalDateTime.now())")
+    @Mapping(target = "statusNotificacao", expression = "java(StatusNotificacao.CANCELADO)")
+    Agendamento paraEntityCancelamento(Agendamento agendamento);
 }
